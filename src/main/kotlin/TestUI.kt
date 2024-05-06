ee1462284643bba3b4a66ceb6d0394546c7410ec
ClausCode <nclaus.code@gmail.com>
2024-05-06 17:35:50 +0300

    chor: UPDATE

--- src/main/kotlin/TestUI.kt
+++ src/main/kotlin/TestUI.kt
@@ -5,10 +5,4 @@ import net.minestom.server.inventory.InventoryType
 
 @UIPreset(path = "ui.yaml", isResource = false)
 class TestUI(viewer: Player) : UI(viewer, InventoryType.CHEST_6_ROW) {
-    init {
-        findComponent<UIButton>("component-id")
-            ?.onAction { viewer.sendMessage(it.id) }
-            ?.withProperty("userId", viewer.username)
-            ?.withProperty("score", viewer.health.toString())
-    }
 }
